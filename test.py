6dfcda8876955a934a63f04f80efb1ee3c2d2741
KIM HAJIN <rlagkwls629@gmail.com>
2022-05-23 14:31:56 +0900

    test 모델

--- test.py
+++ test.py
@@ -1,22 +1,28 @@
-# import tensorflow_hub as hub
-# from tensorflow.keras.models import load_model
-# # from tensorflow.keras import layers
-# from tensorflow.keras.preprocessing.image import ImageDataGenerator
-# import numpy as np
-# import tensorflow as tf
-from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.oaadu.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client.joinsport
-# model = tf.keras.models.load_model('static/model/modetestl.h5')
-doc = {
-    'test' : "test",
-    'hi' : "hi"
-}
-db.joinsport.insert(doc)
-a = db.joinsport.find_one({})
-b = a['photo']
-c = len(b)
+import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow_hub as hub
+from tensorflow.keras.models import load_model
+import cv2
 
-print(a)
-print(b)
-print(c)
+from cv2 import imread, imshow, resize
+
+import tensorflow as tf
+from pymongo import MongoClient
+# client = MongoClient('mongodb+srv://test:sparta@cluster0.oaadu.mongodb.net/Cluster0?retryWrites=true&w=majority')
+# db = client.joinsport
+# model = tf.keras.models.load_model('static/model/modetestl.h5')
+# all = db.joinsport.find_one({'name': "hajin"})
+# name = all['name']
+# print(name)
+
+model = load_model(('static/model/model_pose.h5'), custom_objects={'KerasLayer': hub.KerasLayer})
+image = cv2.imread('static/img/img/5hajin.jpg')
+image = cv2.resize(image,(224,224))
+cv2_imshow = image
+image = np.asarray(image)
+image = image / 255
+image = np.reshape(image, (1, 224, 224, 3))
+print(model.predict(image))
+classes = [np.argmax(model.predict(image))]
+print(classes)
